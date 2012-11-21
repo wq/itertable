dfdf9827dca065ae70b21d876706aa582756c9ba
S. Andrew Sheppard <andrew@wq.io>
2012-11-21 16:59:33 -0600

    use XML feed and request all fields

--- contrib/cocorahs.py
+++ contrib/cocorahs.py
@@ -1,29 +1,30 @@
-from wq.io import CsvNetIO
+from wq.io import XmlNetIO
 from datetime import date, timedelta
 
-class CocorahsIO(CsvNetIO):
+class CocorahsIO(XmlNetIO):
     state     = None
     county    = None
     startdate = date.today() - timedelta(days = 30)
     enddate   = date.today()
-    url       = "http://data.cocorahs.org/cocorahs/export/exportreports.aspx"
     type      = "Daily"
 
+    url       = "http://data.cocorahs.org/cocorahs/export/exportreports.aspx"
     params    = {
         'dtf':            "1",
-        'Format':         "CSV",
+        'Format':         "XML",
         'ReportDateType': "reportdate",
-        'TimesInGMT':     "False"
+        'TimesInGMT':     "False",
+        'responsefields': "all"
     }
+    root_tag = 'Cocorahs'
+    item_tag = 'DailyPrecipReports/DailyPrecipReport'
 
     def load(self):
+        fmt = '%m/%d/%Y'
         self.params['ReportType'] = self.type
         self.params['State']      = self.state
         if self.county is not None:
             self.params['County'] = self.county
-        self.params['StartDate']  = self.datef(self.startdate)
-        self.params['EndDate']    = self.datef(self.enddate)
+        self.params['StartDate']  = self.startdate.strftime(fmt)
+        self.params['EndDate']    = self.enddate.strftime(fmt)
         super(CocorahsIO, self).load()
-
-    def datef(self, date):
-        return '%s/%s/%s' % (date.month, date.day, date.year)
