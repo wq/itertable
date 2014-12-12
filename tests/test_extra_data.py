37e41a8905a2085fe90403c10300d51fad96cf2e
S. Andrew Sheppard <andrew@wq.io>
2014-12-12 15:05:59 +0900

    test extra_data feature

---
+++ tests/test_extra_data.py
@@ -0,0 +1,32 @@
+from wq.io import ExcelFileIO
+from .base import IoTestCase
+import pickle
+from datetime import date
+
+
+class ExtraDataIO(ExcelFileIO):
+    start_row = 5
+
+
+class LoadFileTestCase(IoTestCase):
+    def test_extra_data(self):
+        filename = self.get_filename("extra", "xls")
+        instance = ExtraDataIO(filename=filename)
+        self.check_instance(instance)
+
+        self.assertEqual(instance.extra_data[0][0], "Name")
+        self.assertEqual(instance.extra_data[0][1], "Test")
+        self.assertEqual(instance.extra_data[1][0], "Type")
+        self.assertEqual(instance.extra_data[1][1], "Test")
+        self.assertEqual(instance.extra_data[0][3], "Date")
+        self.assertEqual(instance.extra_data[0][4], date(2014, 12, 12))
+
+    def check_instance(self, instance):
+        self.assertEqual(len(instance), len(self.data))
+
+        for row, data in zip(instance, self.data):
+            for key in data:
+                val = getattr(row, key)
+                if isinstance(val, str) and val.isdigit():
+                    val = int(val)
+                self.assertEqual(val, data[key])
