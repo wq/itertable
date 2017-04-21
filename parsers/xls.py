64a658c4bdb452b19b94e4ffc61fae7979c2b3fc
S. Andrew Sheppard <andrew@wq.io>
2017-04-21 18:21:11 +0000

    format dates, headings & col widths on write

--- parsers/xls.py
+++ parsers/xls.py
@@ -13,6 +13,10 @@ class WorkbookParser(TableParser):
     no_pickle_parser = ['workbook', 'worksheet']
     binary = True
 
+    date_format = 'yyyy-mm-dd'
+    time_format = 'hh:mm:ss'
+    datetime_format = 'yyyy-mm-dd hh:mm:ss'
+
     def parse(self):
         self.parse_workbook()
         if self.sheet_name is None:
@@ -137,19 +141,105 @@ class ExcelParser(WorkbookParser):
                 return datetime.time(*tpl[3:6])
         return cell.value
 
+    def calc_width(self, val):
+        val = str(val) if val is not None else ""
+        size = 0
+        for c in val:
+            if c in ".,;:'\"iIlt1":
+                size += 0.5
+            elif c in 'MW':
+                size += 1.3
+            elif c.isupper():
+                size += 1.2
+            elif c.islower():
+                size += 1
+            else:
+                size += 1.1
+        return size
+
     def open_worksheet(self, file):
         if getattr(self, 'filename', '').endswith('.xls'):
-            import xlwt
-            workbook = xlwt.Workbook()
-            worksheet = workbook.add_sheet('Sheet 1')
-
-            def close():
-                workbook.save(file)
+            return self._open_xls_worksheet(file)
         else:
-            import xlsxwriter
-            workbook = xlsxwriter.Workbook(file)
-            worksheet = workbook.add_worksheet()
+            return self._open_xlsx_worksheet(file)
 
-            def close():
-                workbook.close()
-        return worksheet.write, close
+    def _open_xls_worksheet(self, file):
+        import xlwt
+        workbook = xlwt.Workbook()
+        worksheet = workbook.add_sheet('Sheet 1')
+
+        formats = {
+            datetime.date: xlwt.Style.easyxf(
+                num_format_str=self.date_format,
+            ),
+            datetime.time: xlwt.Style.easyxf(
+                num_format_str=self.time_format,
+            ),
+            datetime.datetime: xlwt.Style.easyxf(
+                num_format_str=self.datetime_format,
+            ),
+            'header': xlwt.Style.easyxf(
+                "font: bold on; borders: bottom thick;"
+            ),
+        }
+
+        widths = {}
+
+        def write(r, c, val):
+            widths.setdefault(c, 0)
+            widths[c] = max(widths[c], self.calc_width(val))
+            fmt = formats.get(type(val))
+            if not fmt and r == 0:
+                fmt = formats['header']
+            if fmt:
+                worksheet.write(r, c, val, fmt)
+            else:
+                worksheet.write(r, c, val)
+
+        def close():
+            for c, width in widths.items():
+                worksheet.col(c).set_width(int(width * 256))
+            workbook.save(file)
+
+        return write, close
+
+    def _open_xlsx_worksheet(self, file):
+        import xlsxwriter
+        workbook = xlsxwriter.Workbook(file)
+        worksheet = workbook.add_worksheet()
+
+        formats = {
+            datetime.date: workbook.add_format({
+                'num_format': self.date_format,
+            }),
+            datetime.time: workbook.add_format({
+                'num_format': self.time_format,
+            }),
+            datetime.datetime: workbook.add_format({
+                'num_format': self.datetime_format,
+            }),
+            'header': workbook.add_format({
+                'bold': True,
+                'bottom': 2,
+            }),
+        }
+        widths = {}
+
+        def write(r, c, val):
+            widths.setdefault(c, 0)
+            widths[c] = max(widths[c], self.calc_width(val))
+
+            fmt = formats.get(type(val))
+            if fmt:
+                worksheet.write_datetime(r, c, val, fmt)
+            elif r == 0:
+                worksheet.write(r, c, val, formats['header'])
+            else:
+                worksheet.write(r, c, val)
+
+        def close():
+            for c, width in widths.items():
+                worksheet.set_column(c, c, width)
+            workbook.close()
+
+        return write, close
