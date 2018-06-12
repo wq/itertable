6bfa10ffaf74b516be5cd7a8ef6019c6386b8d37
S. Andrew Sheppard <andrew@wq.io>
2018-06-12 13:16:47 -0500

    deprecate Python 2.7 support

--- setup.py
+++ setup.py
@@ -120,8 +120,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
